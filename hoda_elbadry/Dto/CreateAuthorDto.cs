84491a03643211622c5a3c19c72776c8239675d4
adam.walid <adam.0522018@gmaqil.com>
2024-11-19 09:04:52 +0200

    snd commit

--- hoda_elbadry/Dto/CreateAuthorDto.cs
+++ hoda_elbadry/Dto/CreateAuthorDto.cs
@@ -5,6 +5,7 @@ namespace hoda_elbadry.Dto
     public class CreateAuthorDto
     {
         //adam
+        //repo
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
     }
